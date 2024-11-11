47e5f8de8849af5e709bb72738db0b2466fee621
neema-s <shrivastavaneema@gmail.com>
2024-11-11 10:14:21 +0530

    add gamemap

--- src/App.js
+++ src/App.js
@@ -9,20 +9,12 @@ import Journal from './pages/Journal';
 function App() {
   return (
     <Routes>
-<<<<<<< HEAD
       {/* <Route path="/" element={<Landing />} />
       <Route path="/login" element={<W0 />} />  
       <Route path="/home" element={<Home />} /> 
       <Route path="/tracker" element={<HabitTracker />} />
       <Route path="/profile" element={<Profile />} /> */}
       <Route path ="/" element={<gamemap/>}/>
-=======
-      <Route path="/" element={<Landing />} />
-      <Route path="/login" element={<W0 />} />  
-      <Route path="/home" element={<Home />} /> 
-      <Route path="/tracker" element={<HabitTracker />} /> 
-      <Route path="/Journal" element={<Journal />} />
->>>>>>> 8b9527292de6d344e6ec09a95f9f8acc7e50dceb
     </Routes>
   );
 }
